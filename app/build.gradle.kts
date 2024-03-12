c9df9f817b1459b348d6ddd58320d95a896f9bca
Harsh Rastogi <harshrastogi013@gmail.com>
2024-03-13 00:18:14 +0530

    dependency done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,13 @@
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+
+    id ("kotlin-parcelize")
+    id ("androidx.navigation.safeargs.kotlin" ) version ("2.5.3")
+
 }
 
 android {
@@ -33,6 +40,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding
+    }
+
 }
 
 dependencies {
@@ -44,4 +55,41 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //HILT
+    implementation ("com.google.dagger:hilt-android:2.33-beta")
+    kapt("com.google.dagger:hilt-android-compiler:2.33-beta")
+    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+
+    //Firebase
+    implementation ("com.google.firebase:firebase-auth:21.0.6")
+
+    //Coroutines with firebase
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.5.1")
+
+    //Android Ktx
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.4.2")
+
+    //stepView
+    implementation ("com.shuhart.stepview:stepview:1.5.1")
+
+    //viewpager2 indicatior
+    implementation ("io.github.vejei.viewpagerindicator:viewpagerindicator:1.0.0-alpha.1")
+
+
+    //loading button
+    implementation ("br.com.simplepass:loading-button-android:2.2.0")
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.13.0")
+    //circular image
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
+    //Navigation component
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.5.2")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.5.2")
+
+    implementation ("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3" )// Update with the latest version if needed
+
+
 }
