8b852f295c20366c71b24b16b61e0217d828e8de
Harsh Rastogi <harshrastogi013@gmail.com>
2024-03-13 20:05:57 +0530

    made packages and added theme and style

---
+++ app/src/main/java/com/example/rms_project/Activities/MainScreen_CustomerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rms_project.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rms_project.R
+
+class MainScreen_CustomerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen_customer)
+    }
+}
---
+++ app/src/main/java/com/example/rms_project/Fragments/loginRegister/IntroductionFragment.kt
@@ -0,0 +1,7 @@
+package com.example.rms_project.Fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.rms_project.R
+
+class IntroductionFragment: Fragment() {
+}
